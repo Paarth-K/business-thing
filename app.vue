3ecbade8dd357b2817693ffc9c238dd854e3567d
Krithin Jay Pakshootra <krithinjaycode@gmail.com>
2023-04-25 18:37:57 +0800

    feat: filling up text

--- app.vue
+++ app.vue
@@ -1,14 +1,11 @@
 <script setup>
 import "/node_modules/primeflex/primeflex.css";
 import "./App.css";
-import "./components/Button.vue";
+import "./components/FillUpText.vue";
 </script>
 
 <template>
   <div>
-    <Button title="hello world" type="primary"></Button>
-    <Button title="hello world" type="danger"></Button>
-    <Button title="hello world" type="info"></Button>
-    <Button title="hello world" type="success"></Button>
+    <FillUpText text="Hello World"></FillUpText>
   </div>
 </template>
---
+++ components/FillUpText.vue
@@ -0,0 +1,36 @@
+<script>
+export default {
+  props: {
+    text: "Hello world",
+  },
+};
+</script>
+
+<template>
+  <h1 :data-text="text">{{ text }}</h1>
+</template>
+
+<style scoped>
+h1 {
+  color: orange;
+  font-size: 3rem;
+  position: absolute;
+  margin: 0;
+  padding: 0;
+}
+
+h1::before {
+  content: attr(data-text);
+  position: absolute;
+  top: 0;
+  left: 0;
+  color: black;
+  overflow: hidden;
+  height: 100%;
+  transition: all 0.5s;
+}
+
+h1:hover::before {
+  height: 0%;
+}
+</style>
