8f92b9bbd39155b36c3d7bb725d89e67dd7f1d58
Paarth Kukreja <paarthkukreja@outlook.com>
2023-04-29 01:42:46 +0800

    basic navbar with cursor interactions and hover sfx

---
+++ components/Nav/Bar.vue
@@ -0,0 +1,38 @@
+<template>
+  <div class="container">
+    <nav-link
+      class="nav-links interactable"
+      v-for="data in navLinkData"
+      :to="data.to"
+      >{{ data.display }}</nav-link
+    >
+  </div>
+</template>
+<script>
+export default {
+  data() {
+    return {
+      navLinkData: [
+        {
+          to: "#about",
+          display: "About",
+        },
+        {
+          to: "#",
+          display: "Pricing",
+        },
+      ],
+    };
+  },
+};
+</script>
+<style scoped>
+.container {
+  position: fixed;
+  background-color: rgba(0, 0, 0, 0);
+  width: 100vw;
+  height: auto;
+  text-align: right;
+  padding: 7px;
+}
+</style>
---
+++ components/Nav/Link.vue
@@ -0,0 +1,44 @@
+<template>
+  <div class="nav-link-container">
+    <nuxt-link :href="to">
+      <button>
+        <slot />
+      </button>
+    </nuxt-link>
+  </div>
+</template>
+
+<script>
+export default {
+  props: ["to"],
+};
+</script>
+
+<style scoped>
+.nav-link-container {
+  display: inline-flex;
+  height: 100%;
+  list-style-type: none;
+  text-align: center;
+  margin-left: 17px;
+  border-radius: 30px;
+  border: 1px dashed;
+}
+.nav-link-container:hover {
+  border: 1px lightcoral dashed;
+}
+
+button {
+  /* filter: invert(); */
+  display: inline-block;
+  padding: 14px;
+  border-radius: 30px;
+  border: none;
+  text-transform: uppercase;
+  transition: all 0.25s;
+}
+
+button:hover {
+  filter: invert();
+}
+</style>
