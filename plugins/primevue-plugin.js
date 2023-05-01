2e2c01beab6e544b8dce921b72a122d20b5499e8
Paarth Kukreja <paarthkukreja@outlook.com>
2023-05-01 19:42:11 +0800

    added a breadcrumb with animaitons, positioning tbd

--- plugins/primevue-plugin.js
+++ plugins/primevue-plugin.js
@@ -1,10 +1,10 @@
 import { defineNuxtPlugin } from "nuxt/app";
 import PrimeVue from "primevue/config";
-import Button from "primevue/button";
-import InputText from "primevue/inputtext";
+import Breadcrumb from "primevue/breadcrumb";
 
 export default defineNuxtPlugin((nuxtApp) => {
   nuxtApp.vueApp.use(PrimeVue);
-  nuxtApp.vueApp.component("Button", Button);
-  nuxtApp.vueApp.component("InputText", InputText);
+  // nuxtApp.vueApp.component("Button", Button);
+  // nuxtApp.vueApp.component("InputText", InputText);
+  nuxtApp.vueApp.component("BreadCrumb", Breadcrumb);
 });
